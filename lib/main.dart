04346c25f368a868be55bc5af7e1c2f468ac7872
Domiciano Rincón <domicianorincon@Domicianos-MacBook-Pro.local>
2026-04-27 11:53:09 -0500

    Semana 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:appmovil261/features/login/ui/bloc/login_bloc.dart';
 import 'package:appmovil261/features/login/ui/screens/login_screen.dart';
-import 'package:appmovil261/navigation/main_screen.dart';
+import 'package:appmovil261/core/navigation/main_screen.dart';
 import 'package:appmovil261/features/signup/ui/bloc/signup_bloc.dart';
 import 'package:appmovil261/features/signup/ui/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
