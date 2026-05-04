abf1f625d798b66d76fdf9fa0c38b64f86c680e3
Domiciano Rincón <domicianorincon@Domicianos-MacBook-Pro.local>
2026-05-04 11:53:15 -0500

    Semana 13

--- lib/features/chat/data/repository/chat_repository_impl.dart
+++ lib/features/chat/data/repository/chat_repository_impl.dart
@@ -1,6 +1,4 @@
 import 'package:appmovil261/features/chat/data/sources/chat_data_source.dart';
-import 'package:appmovil261/features/chat/domain/models/conversation.dart';
-import 'package:appmovil261/features/chat/domain/models/message.dart';
 import 'package:appmovil261/features/chat/domain/repository/chat_repository.dart';
 import 'package:appmovil261/features/profile/domain/model/profile.dart';
 
@@ -10,23 +8,5 @@ class ChatRepositoryImpl implements ChatRepository {
   @override
   Future<List<Profile>> getProfiles(String currentUserId) {
     return _dataSource.getProfiles(currentUserId);
-  }
-
-  @override
-  Future<Conversation> getOrCreateConversation(
-    String currentUserId,
-    String otherUserId,
-  ) {
-    return _dataSource.getOrCreateConversation(currentUserId, otherUserId);
-  }
-
-  @override
-  Future<void> sendMessage(Message message) {
-    return _dataSource.sendMessage(message);
-  }
-
-  @override
-  Stream<List<Message>> watchMessages(String conversationId) {
-    return _dataSource.watchMessages(conversationId);
   }
 }
--- lib/features/chat/domain/repository/chat_repository.dart
+++ lib/features/chat/domain/repository/chat_repository.dart
@@ -1,13 +1,5 @@
-import 'package:appmovil261/features/chat/domain/models/conversation.dart';
-import 'package:appmovil261/features/chat/domain/models/message.dart';
 import 'package:appmovil261/features/profile/domain/model/profile.dart';
 
 abstract class ChatRepository {
   Future<List<Profile>> getProfiles(String currentUserId);
-  Future<Conversation> getOrCreateConversation(
-    String currentUserId,
-    String otherUserId,
-  );
-  Future<void> sendMessage(Message message);
-  Stream<List<Message>> watchMessages(String conversationId);
 }
