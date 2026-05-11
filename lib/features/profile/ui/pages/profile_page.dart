3980c8a46cf6a429dcc41b7fd0f27accc7ba7288
Domiciano Rincón <domicianorincon@Domicianos-MacBook-Pro.local>
2026-05-11 11:58:02 -0500

    Semana 14

--- lib/features/profile/ui/pages/profile_page.dart
+++ lib/features/profile/ui/pages/profile_page.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     final email =
@@ -16,16 +21,36 @@ class ProfilePage extends StatelessWidget {
         if (state is ProfileLogoutSuccessState) {
           Navigator.pushReplacementNamed(context, '/login');
         } else if (state is ProfileErrorState) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(state.message)),
-          );
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.message)));
         }
       },
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.person, size: 64),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 48),
+          const CircleAvatar(
+            radius: 90,
+            child: Icon(Icons.person, size: 128),
+          ),
+          const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.photo_library),
+                  label: const Text('Galería'),
+                ),
+                const SizedBox(width: 12),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.camera_alt),
+                  label: const Text('Cámara'),
+                ),
+              ],
+            ),
             const SizedBox(height: 16),
             Text(email, style: Theme.of(context).textTheme.bodyLarge),
             const SizedBox(height: 32),
@@ -45,7 +70,7 @@ class ProfilePage extends StatelessWidget {
             ),
           ],
         ),
-      ),
     );
   }
 }
+
